83e799d5c78b57f98d5fd5cabc5e63a21b40b826
Antonello Provenzano <antonello@deveel.com>
2016-07-16 15:33:59 +0200

    Replacement of RestShharp with Microsoft.Net.Http to support PCL (and mobile applications)

--- src/OpenAmat.Net/Deveel.OpenAmat.Client/OpenAmatClient.cs
+++ src/OpenAmat.Net/Deveel.OpenAmat.Client/OpenAmatClient.cs
@@ -1,21 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 
-using Deveel.OpenAmat.Client.Serialization;
 using Deveel.OpenAmat.Client.TransitLand.v1;
 
 using DryIoc;
 
 using Newtonsoft.Json;
 
-using RestSharp;
-using RestSharp.Authenticators;
-
 namespace Deveel.OpenAmat.Client {
-	public sealed class OpenAmatClient {
-		private RestClient client;
+	public sealed class OpenAmatClient : IDisposable {
+		private HttpClient client;
 		private OpenAmatClientSettings settings;
 
 		private Container serviceContainer;
@@ -26,6 +27,10 @@ namespace Deveel.OpenAmat.Client {
 
 		public OpenAmatClient(OpenAmatClientSettings settings) {
 			Settings = settings;
+		}
+
+		~OpenAmatClient() {
+			Dispose(false);
 		}
 
 		public OpenAmatClientSettings Settings {
@@ -50,6 +55,20 @@ namespace Deveel.OpenAmat.Client {
 
 		public IStopsManager Stops {
 			get { return serviceContainer.Resolve<IStopsManager>(Settings.SourceType); }
+		}
+
+		private void Dispose(bool disposing) {
+			if (disposing) {
+				if (client != null)
+					client.Dispose();
+			}
+
+			client = null;
+		}
+
+		public void Dispose() {
+			Dispose(true);
+			GC.SuppressFinalize(this);
 		}
 
 		private void AssertConfigured() {
@@ -96,49 +115,100 @@ namespace Deveel.OpenAmat.Client {
 
 			var baseUrl = FindBaseUrl();
 
-			client = new RestClient(baseUrl);
+			client = new HttpClient {
+				BaseAddress = baseUrl
+			};
+		}
 
+		private void SetupAuthentication(HttpRequestMessage request) {
 			if (!String.IsNullOrEmpty(Settings.AuthenticationType)) {
 				switch (Settings.AuthenticationType.ToUpperInvariant()) {
 					case "BASIC": {
-						client.Authenticator = new HttpBasicAuthenticator(Settings.UserName, Settings.Password);
-						break;
-					}
+							request.Headers.Authorization = new AuthenticationHeaderValue("BASIC", CalculateBasicAuth(Settings.UserName, Settings.Password));
+							break;
+						}
 					default:
 						throw new NotSupportedException(String.Format("Authentication type '{0} is not supported.", Settings.AuthenticationType));
 				}
 			}
 		}
 
-		private static void AddParameters(RestRequest request, ParameterType parameterType, object args) {
+		private string CalculateBasicAuth(string userName, string password) {
+			throw new NotImplementedException();
+		}
+
+		private Uri FormRequestUri(string resource, object args, object queryString) {
+			var uriBuilder = new UriBuilder(client.BaseAddress);
+			var path = uriBuilder.Path;
+
 			if (args != null) {
-				var pairs = args.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public)
-					.ToDictionary(key => key.Name, value => value.GetValue(args));
+				var pairs = GetParameters(args);
 				foreach (var pair in pairs) {
-					request.AddParameter(pair.Key, pair.Value, parameterType);
+					var key = new StringBuilder().Append('{').Append(pair.Key).Append('}').ToString();
+					resource = resource.Replace(key, pair.Value.ToString());
 				}
+			}
+
+			if (string.IsNullOrEmpty(path)) {
+				uriBuilder.Path = resource;
+			} else {
+				if (path[path.Length - 1] == '/')
+					path = path.Substring(0, path.Length-1);
+
+				uriBuilder.Path = String.Format("{0}/{1}", path, resource);
+			}
+
+			if (queryString != null) {
+				var qs = GetParameters(queryString);
+				uriBuilder.Query = String.Join("&", qs.Select(pair => String.Format("{0}={1}", pair.Key, pair.Value)));
+			}
+
+			return uriBuilder.Uri;
+		}
+
+		private static Dictionary<string, object> GetParameters(object args) {
+			var pairs = new Dictionary<string, object>();
+			if (args != null) {
+#if PCL
+				pairs = args.GetType().GetRuntimeProperties()
+					.ToDictionary(key => key.Name, value => value.GetValue(args));
+#else
+				pairs = args.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public)
+					.ToDictionary(key => key.Name, value => value.GetValue(args));
+#endif
+			}
+
+			return pairs;
+		}
+
+		private HttpRequestMessage MakeRequest(HttpMethod method, string resource, object args, object queryString = null) {
+			var requestUri = FormRequestUri(resource, args, queryString);
+			var request = new HttpRequestMessage(method, requestUri);
+			SetupAuthentication(request);
+
+			return request;
+		}
+
+		private void SetRequestFormat(HttpRequestMessage request) {
+			if (Settings.RequestFormat == RequestFormat.Json) {
+				request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+			} else if (Settings.RequestFormat == RequestFormat.Xml) {
+				request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("text/xml"));
 			}
 		}
 
 		internal async Task<T> GetAsync<T>(string resource, object args = null, object queryString = null) where T : class {
-			var request = new RestRequest(resource, Method.GET);
-			AddParameters(request, ParameterType.UrlSegment, args);
-			AddParameters(request, ParameterType.QueryString, queryString);
+			var request = MakeRequest(HttpMethod.Get, resource, args, queryString);
 
-			request.RequestFormat = Settings.RequestFormat == RequestFormat.Json ? DataFormat.Json : DataFormat.Xml;
-			request.DateFormat = Settings.DateFormat;
+			SetRequestFormat(request);
 
-			var response = await client.ExecuteTaskAsync(request);
+			var response = await client.SendAsync(request);
 
-			if (response.ResponseStatus == ResponseStatus.TimedOut)
-				throw new TimeoutException("The request timed out");
-			if (response.ResponseStatus == ResponseStatus.Aborted)
-				throw new RequestAbortedException("The request was aborted", response.ErrorException);
-			if (response.ResponseStatus == ResponseStatus.Error)
-				throw new Exception("An error occurred while executing the request", response.ErrorException);
+			response = response.EnsureSuccessStatusCode();
 
 			try {
-				return JsonConvert.DeserializeObject<T>(response.Content, new JsonSerializerSettings {
+				var content = await response.Content.ReadAsStringAsync();
+				return JsonConvert.DeserializeObject<T>(content, new JsonSerializerSettings {
 					ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor,
 					DateFormatString = Settings.DateFormat,
 					DateParseHandling = DateParseHandling.DateTime
